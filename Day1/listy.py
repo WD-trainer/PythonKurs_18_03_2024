d87e7279258d14abed3fc0d5fbc1d45ff9637a67
Wojciech Dudzik <wojtekdudzik@o2.pl>
2024-03-18 10:06:16 +0100

    Zadanie 2

--- Day1/listy.py
+++ Day1/listy.py
@@ -32,3 +32,34 @@ if __name__ == '__main__':
     print(example_rand)
 
     #  Stwórz dwie listy. Każda z list ma zawierać 10 liczb losowych z zakresu 1-10. Wypisz listy na konosole
+    random_list_1 = []
+    random_list_2 = []
+    for i in range(10):
+        random_list_1.append(random.randint(1, 10))
+        random_list_2.append(random.randint(1, 10))
+
+    print(random_list_1)
+    print(random_list_2)
+    ###########################
+
+    lista = [0, 1, 2]
+    lista2 = [3, 4, 5]
+
+    # for el in lista2:
+    #     lista.append(el)
+
+    joined_list = lista + lista2
+    print(joined_list)
+
+    joined_list.remove(3)
+    print(lista)
+
+    random_list_comp = [random.randint(1, 10) for i in range(10)]
+    print(random_list_comp)
+
+    # Napisz kod który umieści w liście 10 kolejnych potęg liczby 2 uzywajac list składanych. 2 ** 2 = 4, 2 ** 3 = 8
+
+
+
+
+
